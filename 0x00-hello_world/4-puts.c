81a64ac8b937b73ebf426598c6733de734041b3c
Drassia <jacknifedragon15@gmail.com>
2023-07-13 02:27:57 -0700

    chmoded all files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ *Hello C
+ */
+int main(void)
+{
+	puts("\"Programing is like building a multilingual puzzle\"");
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+  * main - Enter point
+  * Return: always 0 (success)
+  */
+int main(void)
+{
+	printf("Size of char: %d byte(s)", sizeof(char));
+	printf("Size of an int: %d byte(s)", sizeof(int));
+	printf("Size of a long int: %d byte(s)", sizeof(long int));
+	printf("Size of a long long: %d byte(s)", sizeof(long long));
+	printf("Size of a float: %d byte(s)", sizeof(float));
+	return (0);
+}
