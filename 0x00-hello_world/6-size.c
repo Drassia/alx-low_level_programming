ebf0865d7f3170450743f7ad16f620a2b805fc9b
Drassia <jacknifedragon15@gmail.com>
2023-07-13 05:50:28 -0700

    chmoded all files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
-  * main - Enter point
-  * Return: always 0 (success)
+  * main - A programe that prints the size of various computer types
+  * Return 0 (success)
   */
 int main(void)
 {
