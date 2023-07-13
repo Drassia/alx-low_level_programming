c2a804069a90357a57b024a4bd747c7e68b35b66
Drassia <jacknifedragon15@gmail.com>
2023-07-13 06:27:36 -0700

    chmoded all files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-* main - A program that prints a line with puts function
-* Return: (0) (success)
+*main - A program that prints a line with puts function
+*Return: (0) (success)
 */
 int main(void)
 {
-puts("\"Programing is like building a multilingual puzzle")
+puts("\"Programing is like building a multilingual puzzle");
 return (0);
 }
